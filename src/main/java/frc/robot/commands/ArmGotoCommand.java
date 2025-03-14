a9d941c95d36c29b4a36108774d5d52876b838bf
Nanson Chen <buyingsaucer2@gmail.com>
2025-03-14 12:53:54 -0400

    Cleaned up the logic of the ArmGoToCommand.

--- src/main/java/frc/robot/commands/ArmGotoCommand.java
+++ src/main/java/frc/robot/commands/ArmGotoCommand.java
@@ -4,75 +4,43 @@
 
 package frc.robot.commands;
 
-
 import frc.robot.Constants.ArmPositions;
 import frc.robot.subsystems.ArmSubsystem;
-import frc.robot.subsystems.ElevatorSubsystem;
-
-import com.ctre.phoenix6.StatusSignal;
-import com.ctre.phoenix6.controls.PositionVoltage;
-import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
-/** An example command that uses an example subsystem. */
 public class ArmGotoCommand extends Command {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private ArmSubsystem _armSubsystem;
+  private ArmSubsystem armSubsystem;
   private ArmPositions pos;
-  private StatusSignal<Angle> currentPos;
-
-  private StatusSignal<Double> posError;
 
   /**
    * Creates a new ArmGotoCommand.
    * This command sets the arm positon to the passed in position.
    * 
-   * @param subsystem The ArmSubsystem. 
-   * @param newPos The requested position of the arm. You can find what diffrent positions there are in Constants.java
+   * @param subsystem The ArmSubsystem.
+   * @param newPos    The requested position of the arm. You can find what
+   *                  diffrent positions there are in Constants.java
    */
-  public ArmGotoCommand(ArmSubsystem subsystem, ArmPositions newPos) {
-    
-    _armSubsystem = subsystem;
+  public ArmGotoCommand(ArmSubsystem armSubsystem, ArmPositions newPos) {
+    this.armSubsystem = armSubsystem;
     pos = newPos;
-    posError = _armSubsystem.getPosError();
-    currentPos = _armSubsystem.getActualPos();
-
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+    addRequirements(armSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    _armSubsystem.setTargetPos(pos);
+    armSubsystem.setTargetPos(pos);
   }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    posError.refresh();
-    currentPos.refresh();
-    boolean retVal = false;
+    return armSubsystem.isAtTargetPos();
+  }
 
-        if (pos == ArmPositions.STOWED) {
-            retVal = _armSubsystem.getSwitch();
-        } 
-        else if (currentPos.hasUpdated()) {
-
-            SmartDashboard.putNumber("Arm Pos", currentPos.getValueAsDouble());
-
-            retVal = (Math.abs(currentPos.getValueAsDouble() - (pos.getValue() )) <= 0.1);
-        }
-
-        return retVal;
+  @Override
+  public void end(boolean interrupted) {
+    if (!interrupted)
+      armSubsystem.stopMotor();
   }
 }
